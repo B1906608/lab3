aaa97ab4213da0ada72ad5f1b355989ae1be6e27
Tuong Nguyen <nguyencongtuong2002@gmail.com>
2021-11-03 21:00:27 +0700

    tao trang

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
+import store from "./store";
 import router from "./router";
 
 import { handle } from "./common/promise";
@@ -10,7 +11,8 @@ createApp(App)
     .mixin({
         methods: {
             handle,
-        }
+        },
     })
+    .use(store)
     .use(router)
     .mount('#app');
